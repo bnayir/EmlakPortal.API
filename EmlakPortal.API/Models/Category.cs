7b113de01ff11b979f71a6d8c88443639c9e219a
bnayir <burcnayir@gmail.com>
2026-02-25 22:05:58 +0300

    update

---
+++ EmlakPortal.API/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmlakPortal.Api.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string CategoryName { get; set; }
+
+        public bool Status { get; set; }
+
+        public List<Property> Properties { get; set; }
+    }
+}
---
+++ EmlakPortal.API/Models/Property.cs
@@ -0,0 +1,32 @@
+﻿using EmlakPortal.API.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace EmlakPortal.Api.Models
+{
+    public class Property
+    {
+        [Key]
+        public int PropertyId { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int SquareMeter { get; set; }
+
+        public string RoomCount { get; set; } 
+
+        public string City { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
